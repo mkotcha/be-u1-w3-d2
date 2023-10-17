8210f2caa84d4caa902442b64421fc2c2029e5fa
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-17 14:51:32 +0200

    JpaUtil class

--- src/main/java/emmek/Application.java
+++ src/main/java/emmek/Application.java
@@ -1,12 +1,12 @@
 package emmek;
 
+import emmek.utils.JpaUtil;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class Application {
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("event_management");
-    // "u4d12" DEVE essere il nome dato alla persistence-unit nel file persistence.xml
+    private static final EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
